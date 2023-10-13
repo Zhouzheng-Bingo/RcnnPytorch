d6bf08ed7b3fff42e116c6c9289f385c104cae6d
Zhou Zheng <204784224@qq.com>
2023-10-14 00:17:31 +0800

    能量消耗代码修改
    时间消耗未考虑清楚

--- PowerConsumption.py
+++ PowerConsumption.py
@@ -1,6 +1,6 @@
 import subprocess
 import time
-
+import pywt
 
 def get_cpu_usage():
     ps_command = "Get-Counter -Counter '\\Processor(_Total)\\% Processor Time' -SampleInterval 1 -MaxSamples 1 | " \
@@ -26,16 +26,50 @@ def get_average_metric(metric_function, samples=50):
     return sum(metric_function() for _ in range(samples)) / samples
 
 
-# Measure power while running your neural network task
-start_time = time.time()
-# ... Your neural network task starts here ...
-end_time = time.time()
+# Function to measure energy for a particular task
+def measure_energy_during_task(task, *args, **kwargs):
+    start_time = time.time()
+    cpu_usages, memory_usages, gpu_powers = [], [], []
+    for _ in range(50):  # Repeat the task 50 times
+        cpu_usages.append(get_cpu_usage())
+        memory_usages.append(get_memory_usage())
+        gpu_powers.append(get_gpu_power())
+        task(*args, **kwargs)  # Execute the task
+    end_time = time.time()
+    duration = end_time - start_time
+    avg_cpu_usage = sum(cpu_usages) / len(cpu_usages)
+    avg_memory_usage = sum(memory_usages) / len(memory_usages)
+    avg_gpu_power = sum(gpu_powers) / len(gpu_powers)
 
-average_cpu_usage = get_average_metric(get_cpu_usage)
-average_memory_usage = get_average_metric(get_memory_usage)
-average_gpu_power = get_average_metric(get_gpu_power)
+    print(f"Duration: {duration} seconds")
+    print(f"Average CPU Usage: {avg_cpu_usage}%")
+    print(f"Average Memory Usage: {avg_memory_usage}%")
+    print(f"Average GPU Power: {avg_gpu_power} W")
 
-print(f"Duration: {end_time - start_time} seconds")
-print(f"Average CPU Usage: {average_cpu_usage}%")
-print(f"Average Memory Usage: {average_memory_usage}%")
-print(f"Average GPU Power: {average_gpu_power} W")
+# Example task: Data Preprocessing
+def data_preprocessing(x):
+    x = x[:, :, ::4]
+    A2, D2, D1 = pywt.wavedec(x, 'db4', mode='symmetric', level=2, axis=2)
+
+# Example task: Model Inference (Using TCN as an example)
+def model_inference(model, x):
+    _ = model(x)
+
+
+if __name__ == '__main__':
+    # Initialization and data generation
+    tcn = TCN(1, 1, [4, 8, 16], kernel_size=3, dropout=0)
+    res_net = Net(7, dropout=0)
+    x = torch.rand((1, 7, 20000))
+
+    # Measure energy during data preprocessing
+    print("Measuring during Data Preprocessing...")
+    measure_energy_during_task(data_preprocessing, x)
+
+    # Measure energy during TCN model inference
+    print("Measuring during TCN Model Inference...")
+    measure_energy_during_task(model_inference, tcn, x)
+
+    # Measure energy during ResNet model inference
+    print("Measuring during ResNet Model Inference...")
+    measure_energy_during_task(model_inference, res_net, x)
