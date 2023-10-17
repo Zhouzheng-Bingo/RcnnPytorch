247fd1a78e2abc003f17497ea586d5a23c113e23
Zhou Zheng <204784224@qq.com>
2023-10-17 09:28:05 +0800

    Update measure_energy_function_rasp.py

--- measure_energy_function_rasp.py
+++ measure_energy_function_rasp.py
@@ -6,16 +6,15 @@ def get_cpu_temp():
     result = subprocess.run(['vcgencmd', 'measure_temp'], stdout=subprocess.PIPE)
     return float(result.stdout.decode('utf-8').split('=')[1].split('\'')[0])
 
-
 def get_cpu_usage():
-    result = subprocess.run(['awk', '-F\'[ :%]*\'', '\'NF{if ($2>0) print $3}\'', '/proc/stat'], stdout=subprocess.PIPE)
+    cmd = ["awk", "-v", "RS=''", "{print 1-($5*1.0)/($2+$3+$4+$5+$6+$7+$8)}", "/proc/stat"]
+    result = subprocess.run(cmd, stdout=subprocess.PIPE)
     return float(result.stdout.decode('utf-8').strip())
-
 
 def get_memory_usage():
-    result = subprocess.run(['free', '|', 'awk', '\'FNR == 3 {print $3/($3+$4)*100}\''], stdout=subprocess.PIPE)
+    cmd = ["awk", "FNR == 2 {print $3/($3+$4)}"]
+    result = subprocess.run(["free", "|"] + cmd, stdout=subprocess.PIPE)
     return float(result.stdout.decode('utf-8').strip())
-
 
 def get_gpu_power():
     return -1.0
